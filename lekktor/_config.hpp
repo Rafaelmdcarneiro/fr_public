66c6caa125ed19cf24a5d74d78c709f7ae79a3f3
Rafael Carneiro <rafaelmdcarneiro@gmail.com>
2024-06-26 11:27:52 -0300

    Add files via upload

---
+++ lekktor/_config.hpp
@@ -0,0 +1,16 @@
+// This file is distributed under a BSD license. See LICENSE.txt for details.
+
+
+
+/****************************************************************************/
+/***                                                                      ***/
+/***   System Configuration                                               ***/
+/***                                                                      ***/
+/****************************************************************************/
+
+// Compile Options
+
+#define sINTRO                  0 // compile for small size
+#define sDEBUG                  1 // include debug code even in release build
+#define sAPPNAME    "lekktor"     // application name for window title
+#define sVERSION    "1.00"        // version number string
---
+++ lekktor/parse.hpp
@@ -0,0 +1,9 @@
+// This file is distributed under a BSD license. See LICENSE.txt for details.
+
+#pragma once
+
+void ParseDecl();
+void Parse();
+
+void Match(sInt tok);
+void Match();
